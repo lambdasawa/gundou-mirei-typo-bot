93b0c3b842606d8c3b644789a31218efa0292ae3
Tsubasa Irisawa <lambdasawa@gmail.com>
2020-04-12 03:33:32 +0900

    :rocket: Use serverless framework
    :wrench: Setup timeout parameter

--- main.go
+++ main.go
@@ -1,15 +1,10 @@
 package main
 
 import (
-	"encoding/base64"
 	"os"
 
 	"github.com/aws/aws-lambda-go/lambda"
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/kms"
 	"github.com/lambdasawa/gundou-mirei-typo-bot/tw"
-	"github.com/pkg/errors"
 	log "github.com/sirupsen/logrus"
 )
 
@@ -31,51 +26,15 @@ func isAWSLambdaEnv() bool {
 }
 
 func GetLambdaHandler() func() error {
-	kssService := kms.New(session.New(), aws.NewConfig().WithRegion("ap-northeast-1"))
-
 	conf := tw.TwitterConfig{}
-	var err error
-
-	conf.ConsumerKey, err = decodeKMS(kssService, "CONSUMER_KEY")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	conf.ConsumerSecret, err = decodeKMS(kssService, "CONSUMER_SECRET")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	conf.AccessToken, err = decodeKMS(kssService, "ACCESS_TOKEN")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	conf.AccessSecret, err = decodeKMS(kssService, "ACCESS_SECRET")
-	if err != nil {
-		log.Fatal(err)
-	}
+	conf.ConsumerKey = os.Getenv("CONSUMER_KEY")
+	conf.ConsumerSecret = os.Getenv("CONSUMER_SECRET")
+	conf.AccessToken = os.Getenv("ACCESS_TOKEN")
+	conf.AccessSecret = os.Getenv("ACCESS_SECRET")
 
 	return func() error {
 		return tw.Favorite(conf)
 	}
-}
-
-func decodeKMS(svc *kms.KMS, envName string) (string, error) {
-	dataBytes, err := base64.StdEncoding.DecodeString(os.Getenv(envName))
-	if err != nil {
-		return "", errors.Wrap(err, "failed to decode KMS data as Base64")
-	}
-
-	var in = &kms.DecryptInput{
-		CiphertextBlob: dataBytes,
-	}
-	out, err := svc.Decrypt(in)
-	if err != nil {
-		return "", errors.Wrap(err, "failed to decrypt KMS value")
-	}
-
-	return string(out.Plaintext), nil
 }
 
 func start() {
